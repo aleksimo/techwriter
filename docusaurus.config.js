acbdaabd266169a1ac903d97448cb62493924f51
Aleksei Simonenko <asimonenko@workfusion.com>
2023-07-26 09:45:04 +0200

    Updated naming

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   trailingSlash: false,
 
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -120,7 +120,7 @@ const config = {
             position: 'right',
             sidebarId: 'Examples',
             label: 'Portfolio',
-            to: '/examples'
+            to: '/portfolio'
 
           },
           {
